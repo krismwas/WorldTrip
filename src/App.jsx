cad6988504e1568a5409a4efa590d8864376bb27
kris <krismwasgit@gmail.com>
2024-12-17 14:50:32 +0300

    cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useReducer, useState } from "react";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login.jsx";
+import CityList from "./components/CityList.jsx";
+
+const BASE_URL = `http://localhost:8000`;
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -14,8 +38,14 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>list of cities</p>} />
-          <Route path="cities" element={<p>list of cities</p>} />
+          <Route
+            index
+            element={<CityList isLoading={isLoading} cities={cities} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList isLoading={isLoading} cities={cities} />}
+          />
           <Route path="countries" element={<p>countries</p>} />
           <Route path="form" element={<p>form</p>} />
         </Route>
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,5 @@
+function CityItem({ city }) {
+  return <div>city</div>;
+}
+
+export default CityItem;
