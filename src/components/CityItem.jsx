b52e118d4c5c4ca5352d59e517673bb757fb7a16
kris <krismwasgit@gmail.com>
2024-12-17 15:21:36 +0300

    city presentation

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,23 @@
+import styles from "./CityItem.module.css";
+
 function CityItem({ city }) {
-  return <div>city</div>;
+  const { cityName, emoji, date } = city;
+
+  const formatDate = (date) =>
+    new Intl.DateTimeFormat("en", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }).format(new Date(date));
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
 }
 
 export default CityItem;
